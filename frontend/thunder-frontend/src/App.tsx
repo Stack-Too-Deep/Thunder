f1bf5fee59b621de6b16b512b4d1668fc6b629b8
vrag99 <garv.codes@gmail.com>
2024-01-13 19:36:47 +0530

    setup a test route for zkp integration

--- frontend/thunder-frontend/src/App.tsx
+++ frontend/thunder-frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./components/pages/home/home";
+import CreditScoreTest from "./components/pages/credit-score-test/credit-score-test";
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <div className="grid place-items-center text-4xl h-screen bg-speedline">
-            Vite is awesome !!
-          </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/credit-score-test" element={<CreditScoreTest />} />
+        </Routes>
       </ThemeProvider>
     </>
   );
